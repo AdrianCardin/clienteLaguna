bd9ed90bb30e1ca52174c703308335f77e7e50cb
AdrianCardin <acardinlozano@gmail.com>
2021-11-14 18:06:17 +0100

    preparando apuntes para examen 2

--- declaraciones/string.js
+++ declaraciones/string.js
@@ -10,3 +10,15 @@ cadena.toLowerCase();
 //split  
 //El método split() divide un objeto de tipo String en un array (vector) de cadenas mediante la separación de la cadena en subcadenas.
 var arrayDeCadenas = cadenaADividir.split(separador);
+
+// ultimo indice
+String.prototype.lastIndexOf(searchValue[, fromIndex]);
+
+//replace 
+String.prototype.replace(searchFor, replaceWith);
+
+//
+var cadena1 = "La mañana se nos echa encima.";
+var cadena2 = cadena1.slice(3, -2);
+console.log(cadena2);
+// mañana se nos echa encim
