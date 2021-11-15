bb521e3c56d9b03665abdbdce8e278f654a49643
AdrianCardin <acardinlozano@gmail.com>
2021-11-15 20:55:14 +0100

    preparado apuntes para examen

---
+++ declaraciones/formularios.js
@@ -0,0 +1,8 @@
+//obtener valor de un formulario
+var resultadoPractica1=Number(document.getElementById("practica1").value) ;
+
+//escribit en el documento sobreescribiendo todo
+document.write();
+
+//escribir en el documento sin sobreescribir
+resultadoPractica1.innerHTML="";
