bd6044bd6aa69265829282453a267f92a8a7a448
AdrianCardin <acardinlozano@gmail.com>
2021-12-09 14:05:28 +0100

    ST U5 EJ3 Hecho!!!

--- segundoTrimestre/U5/ejercicio3/funciones.js
+++ segundoTrimestre/U5/ejercicio3/funciones.js
@@ -1,6 +1,6 @@
 function desabilitar() {
     marcas=["Ferrari","Porsche","Audi","Mercedes"];
-
+    console.log("a");
     for (let i = 0; i < marcas.length; i++) {
       document.getElementById(marcas[i]).disabled = false;
     }
@@ -15,22 +15,24 @@ function desabilitar() {
 
   function pintar() {
       array=[];
-      arrayNombres=["fecha", "rango" , "telefono","color","Marca Coche","Modelo Coche" ];
+      arrayNombres=["fecha", "rango" , "telefono","color","Marca deshabilitado","Marca Coche","Modelo Coche" ];
       array.push(fecha=document.formulario.fecha.value);
       array.push(rango=document.formulario.rango.value);
       array.push(telefono=document.formulario.telefono.value);
       array.push(color=document.formulario.color.value);
+      array.push(MArcaDesabilitada=document.formulario.cocheUno.value);
       IndiceMarcaCoche=document.formulario.cocheDos.selectedIndex;
       array.push(MarcaCoche=document.formulario.cocheDos.options[IndiceMarcaCoche].parentNode.label);
       array.push(ModeloCoche=document.formulario.cocheDos.options[IndiceMarcaCoche].value);
 
       var pintar=document.getElementById("pintar");
       pintar.innerHTML="";
-      debugger;
+      
       for (let i = 0; i < array.length; i++) {
       pintar.innerHTML +=arrayNombres[i] + " -> "+ array[i] + '</br>';
           
       }
   }
 
-document.getElementById("formulario").addEventListener("onchange", pintar, false);
+  document.formulario.enviar.addEventListener("click", pintar, false);
+  document.getElementById("cocheUno").addEventListener("change", desabilitar, false);
